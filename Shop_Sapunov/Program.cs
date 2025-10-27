d9a5c810f35bac4ca591b9d7a440f7eed7f417e6
Sapunov-danil <sapunov.daniil13@gmail.com>
2025-10-27 23:37:52 +0500

    Разработанный контроллер предметов.  Сапунов

--- Shop_Sapunov/Program.cs
+++ Shop_Sapunov/Program.cs
@@ -1,4 +1,9 @@
+using Shop_Sapunov.Data.Interfaces;
+using Shop_Sapunov.Data.Mocks;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<ICategorys, MockCategorys>();
+builder.Services.AddTransient<IItems, MockItems>();
 builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
 var app = builder.Build();
 
