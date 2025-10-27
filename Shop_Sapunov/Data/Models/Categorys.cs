38ca950199479f2c6c765c3827b518d81243ad2e
Sapunov-danil <sapunov.daniil13@gmail.com>
2025-10-27 23:22:05 +0500

    Разработанные модели данных. Сапунов

---
+++ Shop_Sapunov/Data/Models/Categorys.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop_Sapunov.Data.Models
+{
+    public class Categorys
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Items> Items { get; set; }
+    }
+}
