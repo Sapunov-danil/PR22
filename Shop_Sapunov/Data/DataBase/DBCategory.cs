b72e860d6be6ec30ce9ab4d5fc73c41b87c1d4f1
Sapunov-danil <sapunov.daniil13@gmail.com>
2025-10-28 14:04:45 +0500

    Получение данных о категориях и предметах. Сапунов

---
+++ Shop_Sapunov/Data/DataBase/DBCategory.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using Shop_Sapunov.Data.Common;
+using Shop_Sapunov.Data.Interfaces;
+using Shop_Sapunov.Data.Models;
+
+namespace Shop_Sapunov.Data.DataBase
+{
+    public class DBCategory : ICategorys
+    {
+        public IEnumerable<Categorys> AllCategorys
+        {
+            get
+            {
+                List<Categorys> categories = new List<Categorys>();
+                MySqlConnection MySqlConnection = Connection.MySqlOpen();
+                MySqlDataReader CategorysData = Connection.MySqlQuery("SELECT * FROM Shop.Categorys ORDER BY `Name`;", MySqlConnection);
+                while (CategorysData.Read())
+                {
+                    categories.Add(new Categorys()
+                    {
+                        Id = CategorysData.IsDBNull(0) ? -1 : CategorysData.GetInt32(0),
+                        Name = CategorysData.IsDBNull(1) ? null : CategorysData.GetString(1),
+                        Description = CategorysData.IsDBNull(2) ? null : CategorysData.GetString(2)
+                    });
+                }
+                return categories;
+            }
+        }
+    }
+}
---
+++ Shop_Sapunov/Data/DataBase/DBItems.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using Shop_Sapunov.Data.Common;
+using Shop_Sapunov.Data.Interfaces;
+using Shop_Sapunov.Data.Models;
+
+namespace Shop_Sapunov.Data.DataBase
+{
+    public class DBItems : IItems
+    {
+        public IEnumerable<Categorys> Categorys = new DBCategory().AllCategorys;
+
+        public IEnumerable<Items> AllItems
+        {
+            get
+            {
+                List<Items> items = new List<Items>();
+                MySqlConnection MySqlConnection = Connection.MySqlOpen();
+                MySqlDataReader ItemsData = Connection.MySqlQuery("SELECT * FROM Shop.Items ORDER BY `Name`;", MySqlConnection);
+                while (ItemsData.Read())
+                {
+                    items.Add(new Items()
+                    {
+                        Id = ItemsData.IsDBNull(0) ? -1 : ItemsData.GetInt32(0),
+                        Name = ItemsData.IsDBNull(1) ? null : ItemsData.GetString(1),
+                        Description = ItemsData.IsDBNull(2) ? null : ItemsData.GetString(2),
+                        Img = ItemsData.IsDBNull(3) ? null : ItemsData.GetString(3),
+                        Price = ItemsData.IsDBNull(4) ? -1 : ItemsData.GetInt32(4),
+                        Category = ItemsData.IsDBNull(5) ? null : Categorys.Where(x => x.Id == ItemsData.GetInt32(5)).First()
+                    });
+                }
+                MySqlConnection.Close();
+                return items;
+            }
+        }
+    }
+}
