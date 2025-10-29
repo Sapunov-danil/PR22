bdb9d4f90eabe8552c6635facd8672d42f6804d1
Sapunov-danil <sapunov.daniil13@gmail.com>
2025-10-29 08:21:49 +0500

    Разработанное перенаправление на страницу с предметами. Сапунов

---
+++ Shop_Sapunov/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop_Sapunov.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RedirectResult Index() 
+        {
+            return Redirect("/Items/List");
+        }
+    }
+}
